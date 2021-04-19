0ddcccde4c7a5c5d5ba22cf9f94fa700118f26ab
Kazım Muhammet Temiz <kzmtmz52@gmail.com>
2021-04-19 21:19:35 +0300

    initial commit

---
+++ app/src/main/java/com/kazim/homeworkanimalfinder/Level1.kt
@@ -0,0 +1,11 @@
+package com.kazim.homeworkanimalfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Level1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level1)
+    }
+}
